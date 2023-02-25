f4fb194d5a5b9eb333d15f7fdc1ee0fdce1f3ce1
Samir Karimov <samir.karimov.01@gmail.com>
2023-02-26 00:45:28 +0300

    Четвертая задача решена

--- Program.cs
+++ Program.cs
@@ -1,8 +1,22 @@
 ﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// Console.Write("Введите первое число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// if (number1 > number2) Console.WriteLine("{0} больше {1}",number1,number2);
+// else Console.WriteLine("{1} больше {0}",number1,number2);
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 Console.Write("Введите первое число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
 
-if (number1 > number2) Console.WriteLine("{0} больше {1}",number1,number2);
-else Console.WriteLine("{1} больше {0}",number1,number2);
+int result = number1;
+
+if (number2 > result) result = number2;
+if (number3 > result) result = number3;
+Console.WriteLine("Максимальное число - {0}",result);
